77eb18d44d743646d52af554e8fb1a5dd7207a4d
m7kor <Alshariefk@gmail.com>
2026-08-19 02:05:58 +0300

    Fix HTTP status code for method not allowed

--- api/upload.js
+++ api/upload.js
@@ -5,7 +5,7 @@
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(455).json({ error: 'Method Not Allowed' });
+    return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
   const {
