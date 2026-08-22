97022034c0d4580d671ae474f9c6f68516c5c2a9
b7be <b7be@local>
2026-08-22 23:35:53 +0300

    Fix all 7 bugs: purge stale cache v5, fix corrupted TV data, create sw.js self-destruct, fix CSP connect-src, remove CSS !important conflict

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Service Worker Self-Destruct & Cache Buster
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
+  );
+});
+
+// Do NOT intercept any fetch events!
